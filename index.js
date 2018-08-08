d77e89ba901910ee97fda5b77a0fe6e6566d2f26
pwelagedara <pubuduwelagedara@gmail.com>
2018-08-08 14:05:38 -0400

    100

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ module.exports = function() {
 
     return new Promise(function(resolve, reject) {
 
-        if(1 > 2) {
-            console.log('this will never be executed');
-        }
+        // if(1 > 2) {
+        //     console.log('this will never be executed');
+        // }
         
         return resolve('okay');
     });
